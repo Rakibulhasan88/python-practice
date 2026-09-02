14e185efd738aea06694e834d9095b6dafe396e4
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-02 23:10:35 +0600

    Break Statement

--- Loop.py
+++ Loop.py
@@ -3,6 +3,14 @@
 # for item in a:
 #     print(item)
 
-for i in range(10):
-    print(f"Loading... {i}%")
-print("Loading Complete!")
+# for i in range(10):
+#     print(f"Loading... {i}%")
+# print("Loading Complete!")
+
+a = [1, 2, 3, 4, "a", 5, 6, 7, 8, 9, 10]
+
+for i in a:
+    if type(i) == str:
+        break
+    else:
+        print(i)
