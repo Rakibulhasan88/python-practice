e46d8b4351be95fe0b7eb8e50b7028e47022458a
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-02 22:14:45 +0600

    Range Method

--- List.py
+++ List.py
@@ -1,6 +1,15 @@
-a = [1, 2, 3, 'Rakib', 'Fahim', 8.9, 3.4]
-print(a[0])
-print(a[-1]) # Last element of List
+#a = [1, 2, 3, 'Rakib', 'Fahim', 8.9, 3.4]
+# print(a[0])
+# print(a[-1]) # Last element of List
 
-s = "Hello World"
-print(list(s)) # Convert string to list
+# s = "Hello World"
+# print(list(s)) # Convert string to list
+# a.append('Hasan')
+# print(a)
+
+a = list(range(10))
+b = tuple(range(10))
+c = list(range(1, 10, 2))
+print(a)
+print(b)
+print(c)
