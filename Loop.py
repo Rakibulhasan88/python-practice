dcd75de369891601d94e707a29575ac606c6fb53
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-03 10:54:08 +0600

    List Comprehension

--- Loop.py
+++ Loop.py
@@ -34,3 +34,19 @@ for i in a:
     if i%2 == 0:
         result.append(i)
 print(result)
+
+#List Comprehension
+new_result = [i for i in a if i%2 == 0]
+print(new_result)
+
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+b_new_result = []
+for i in b:
+    if i%2 == 0:
+        b_new_result.append(i**2)
+    else:
+        b_new_result.append(i)
+print(b_new_result)
+
+b_new = [i**2 if i%2 == 0 else i for i in b]
+print(b_new)
