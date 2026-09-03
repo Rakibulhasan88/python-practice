b4765ed256e89b53ab74a6e8419990e06cb78aad
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-03 13:58:37 +0600

    While Loop

--- Loop.py
+++ Loop.py
@@ -25,28 +25,38 @@
 
 #List Comprehension
 
+# a=[1,10,23,24,90,100,200,300,400,500]
+
+# result = []
+
+# # Normal way
+# for i in a:
+#     if i%2 == 0:
+#         result.append(i)
+# print(result)
+
+# #List Comprehension
+# new_result = [i for i in a if i%2 == 0]
+# print(new_result)
+
+# b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# b_new_result = []
+# for i in b:
+#     if i%2 == 0:
+#         b_new_result.append(i**2)
+#     else:
+#         b_new_result.append(i)
+# print(b_new_result)
+
+# b_new = [i**2 if i%2 == 0 else i for i in b]
+# print(b_new)
+
 a=[1,10,23,24,90,100,200,300,400,500]
+result = 0
 
-result = []
-
-# Normal way
-for i in a:
-    if i%2 == 0:
-        result.append(i)
+i = 0
+n = len(a)
+while i < n:
+    result = result + a[i]
+    i = i + 1
 print(result)
-
-#List Comprehension
-new_result = [i for i in a if i%2 == 0]
-print(new_result)
-
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-b_new_result = []
-for i in b:
-    if i%2 == 0:
-        b_new_result.append(i**2)
-    else:
-        b_new_result.append(i)
-print(b_new_result)
-
-b_new = [i**2 if i%2 == 0 else i for i in b]
-print(b_new)
