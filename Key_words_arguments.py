1702e32ce82f072161bf49dcff63aedbad0ca792
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-05 09:49:27 +0600

    Key words arguments

--- Key_words_arguments.py
+++ Key_words_arguments.py
@@ -1,5 +1,13 @@
-def my_func(f_name, l_name, age):
-    print(f"First name: {f_name}, Last name: {l_name}, Age: {age} years old.")
+# def my_func(f_name, l_name, age):
+#     print(f"First name: {f_name}, Last name: {l_name}, Age: {age} years old.")
 
-# my_func("John", "Doe", 30)
-my_func(age = 30, l_name = "Doe", f_name = "John")
+# # my_func("John", "Doe", 30)
+# my_func(age = 30, l_name = "Doe", f_name = "John")
+
+# Arbitary Number of key word arguments
+
+def my_func(**kwargs):
+    print(kwargs)
+    print(f"My name is {kwargs['f_name']} {kwargs['l_name']}, I am {kwargs['age']} years old. I got {kwargs['marks']} marks in my last exam.")
+    
+my_func(age = 30, l_name = "Doe", f_name = "John", marks =90)
