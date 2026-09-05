7b013a4e2ab02cb485e77c17f63a1c56e9a38767
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-05 20:37:38 +0600

    Lambda Function

---
+++ Lambda_function.py
@@ -0,0 +1,11 @@
+#anonymous functions --> Unnamed functions
+
+# def square(x):
+#     return x * x
+# print(square(5))
+
+square = lambda x: x * x
+print(square(4))
+
+add = lambda a, b : a + b
+print(add(5, 6))
