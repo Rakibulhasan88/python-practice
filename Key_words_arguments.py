93853d49456752cafae5b5089862787084664d3b
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-05 09:38:17 +0600

    Key_words_aguments

---
+++ Key_words_arguments.py
@@ -0,0 +1,5 @@
+def my_func(f_name, l_name, age):
+    print(f"First name: {f_name}, Last name: {l_name}, Age: {age} years old.")
+
+# my_func("John", "Doe", 30)
+my_func(age = 30, l_name = "Doe", f_name = "John")
