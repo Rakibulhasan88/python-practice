64701dc7272486bb764c9d8b99cf926909ce629b
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-05 08:37:22 +0600

    Calculator Using Function

--- function.py
+++ function.py
@@ -5,5 +5,28 @@
 
 # print(info())
 
-mx = max([1, 2, 3, 4, 5])
-print(f"Maximun value is: {mx}. {mx*3}")
+# mx = max([1, 2, 3, 4, 5])
+# print(f"Maximun value is: {mx}. {mx*3}")
+
+def Calculator():
+    num1 = int(input("Enter first number:"))
+    num2 = int(input("Enter second number:"))
+    operation = input("Enter operation (+, -, *, /):")
+    
+    if operation == "+":
+        return num1 + num2
+    elif operation == "-":
+        return num1 - num2
+    elif operation == "*":
+        return num1 * num2
+    elif operation == "/":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero is not allowed."
+    else:
+        return "Error: Invalid operation."
+    
+result = Calculator()
+print(f"Result: {result}")
+    
