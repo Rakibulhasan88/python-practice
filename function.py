00db73425246e000580ce5474487b69e81aec295
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-05 08:25:05 +0600

    Function

---
+++ function.py
@@ -0,0 +1,9 @@
+# def info():
+#     name = input("Enter your name: ")
+#     age = input("Enter your age: ")
+#     return f"Name: {name}, Age: {age}"
+
+# print(info())
+
+mx = max([1, 2, 3, 4, 5])
+print(f"Maximun value is: {mx}. {mx*3}")
