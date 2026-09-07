1b40ed7daf600efab4c3e4b0507a6347a1641f11
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-07 10:47:32 +0600

    File Read & Write

--- file_handling.py
+++ file_handling.py
@@ -1,27 +1,16 @@
-# file=open('file.txt','r')
-# content=file.read()
-# print(content)
+# For file open
+file = open('file.txt', 'r')
+content = file.read()
+print(content)
 
-# file.close()
+file.close() #for file close
 
-# with open('file.txt','r') as f:
-#     content=f.read()
-#     print(content)
+# Professional way to open a file using 'with' statement
+with open('file.txt', 'r') as f:
+    content = f.read()
+    print(content)
     
-# with open('file.txt','w') as f:
-#     f.write('\n Hello World\n')
-#     f.write("i am Rakibul Hasan\n")
-
-import os
-import pathlib
-if os.path.exists('file.txt'):
-    print("This file exists")
-else:
-    print("This file does not exist")
-
-file_path= pathlib.Path('file.txt')
-
-if file_path.exists():
-    print("this file exists")
-print(os.path.abspath('file.txt'))
-print(os.path.getsize('file.txt'))
+# Professional way to write to a file using 'with' statement
+with open('file.txt', 'w') as f:
+    f.write("This is a sample text written to the file.")
+    
