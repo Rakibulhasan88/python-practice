d8f9fd532714b7ed77e07daae981e1b7b7f22ebb
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-08 07:37:22 +0600

    Exception Handling

--- Exception_handling.py
+++ Exception_handling.py
@@ -3,9 +3,21 @@
 # Errors --> Compile Time (Syntex, Indentation)
 # Exceptions  --> Run Time Error (Indexing, key, value, zero division)
 
-
 try:
     with open("file.txt", 'r') as f:
         print(f.read())
+    print(10/0)
+    x = int("abc")
+    a = [1, 2, 3]
+    print(a[100])
+    x = abc
+except ZeroDivisionError:
+    print("Error : Division by zero is not possible.")
 except FileNotFoundError:
     print("File Not Found")
+except ValueError:
+    print("Invalid Value")
+except IndexError:
+    print("Invalid Index")
+except Exception as e:
+    print("Sone error occurred!!", e)
