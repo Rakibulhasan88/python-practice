370a073220769d3f212cd9663c4ae446d0d94b4a
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-10 21:29:14 +0600

    Protected Members

--- encapsulation.py
+++ encapsulation.py
@@ -1,9 +1,22 @@
-# Public Members
-class Employee:
-    def __init__(self, name, salary):
-        self.name = name
-        self.salary = salary
+# # Public Members
+# class Employee:
+#     def __init__(self, name, salary):
+#         self.name = name
+#         self.salary = salary
         
-emp = Employee('Rakib', 5000)
-print(emp.name)
-print(emp.salary)
+# emp = Employee('Rakib', 5000)
+# print(emp.name)
+# print(emp.salary)
+
+# Protected Members
+class Company:
+    def __init__(self):
+        self._project = "AI System" #Protected Attribute
+        
+class TechCompany(Company):
+    def show_project(self):
+        print(f'working on: {self._project}')
+        
+comp = TechCompany()
+comp.show_project()
+print(comp._project)
