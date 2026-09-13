666bec149806b8f1ac1c888f1e8178bb6ab30e37
Rakibulhasan88 <rakibalhasan488@gmail.com>
2026-09-13 21:32:42 +0600

    Factory Design Pattern

--- design_pattern.py
+++ design_pattern.py
@@ -1,14 +1,36 @@
-# Singleton
+# # 1.Singleton
 
-class Singleton:
-    _instance = None #class variable
+# class Singleton:
+#     _instance = None #class variable
     
-    def __new__(cls):
-        if cls._instance is None:
-            print("1st obj is created")
-            cls._instance = super(Singleton, cls).__new__(cls)
+#     def __new__(cls):
+#         if cls._instance is None:
+#             print("1st obj is created")
+#             cls._instance = super(Singleton, cls).__new__(cls)
             
-ob1 = Singleton()
-ob2 = Singleton()
+# ob1 = Singleton()
+# ob2 = Singleton()
 
-print(ob1 is ob2)
+# print(ob1 is ob2)
+
+# 2. Factory Design System
+class Car:
+    def driver(self):
+        return "Driving a car"
+    
+class Bike:
+    def driver(self):
+        return "Riding a bike"
+    
+class VehicleFactory:
+    @staticmethod
+    def get_vehicle(type):
+        if type == "car":
+            return Car()
+        elif type == "bike":
+            return Bike()
+        else:
+            return ValueError("Unknown bike")
+        
+vehicle = VehicleFactory.get_vehicle("Car")
+print(vehicle.driver())
